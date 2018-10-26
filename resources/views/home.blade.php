56ba6716506f87a0fbdad35390df37ea89f7ac8d
Zubatzx <adityaputrabudiman@yahoo.co.id>
2018-10-26 12:59:18 +0700

    home and layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,266 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<section class="engine"><a href="https://mobirise.me/r">free bootstrap templates</a></section><section class="cid-r5NgUy0haP mbr-fullscreen mbr-parallax-background" id="header2-2">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    
+
+    
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1"></h1>
+                
+                <p class="mbr-text pb-3 mbr-fonts-style display-5"><br><br><br><br><br><br><br><br></p>
+                <div class="mbr-section-btn"><a class="btn btn-md btn-primary-outline display-4" href="https://mobirise.com">Start KonText</a>
+                    <a class="btn btn-md btn-primary-outline display-4" href="https://mobirise.com">Start KonFace</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section>
+
+<section class="features12 cid-r5Nk7mT9c9" id="features12-5">
+    
+    
+
+    
+
+    <div class="container">
+        <h2 class="mbr-section-title pb-2 mbr-fonts-style display-2">
+            Why KonsOL ?</h2>
+        <h3 class="mbr-section-subtitle pb-3 mbr-fonts-style display-5"></h3>
+
+        <div class="media-container-row pt-5">
+            <div class="block-content align-right">
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                             <span class="mbri-upload mbr-iconfont"></span>
                         </div>
-                    @endif
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                                Host Anywhere                     
+                            </h4>
+                        </div>
+                    </div>                
 
-                    You are logged in!
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Publish your website to a local drive, FTP or host on Amazon S3, Google Cloud, Github Pages. Don't be a hostage to just one platform or service provider.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                            <span class="mbri-drag-n-drop2 mbr-iconfont"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                                Easy and Simple to Use
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Just drop the blocks into the page, edit content inline and publish - no technical skills required.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mbr-figure" style="width: 50%;">
+                <img src="assets/images/06.jpg" alt="Mobirise">
+            </div>
+
+            <div class="block-content align-left  ">
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                             <span class="mbri-features mbr-iconfont"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                                Over 400 Amazing Blocks
+                            </h4>
+                        </div>
+                    </div>                
+
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Mobirise offers several themes that include sliders, galleries, article blocks, counters, accordions, video and many more.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="card pl-3 pr-3 pb-5">
+                    <div class="mbr-card-img-title">
+                        <div class="card-img pb-3">
+                            <span class="mbri-sites mbr-iconfont"></span>
+                        </div>
+                        <div class="mbr-crt-title">
+                            <h4 class="card-title py-2 mbr-crt-title mbr-fonts-style display-7">
+                                Unlimited Sites
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="card-box">
+                        <p class="mbr-text mbr-section-text mbr-fonts-style display-7">
+                            Mobirise gives you the freedom to develop as many websites as you like given the fact that it is a desktop app.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</section>
+
+<section class="testimonials2 cid-r5NFZrMDz9" id="testimonials2-h">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="mbr-figure pr-lg-5" style="width: 130%;">
+              <img src="assets/images/face4.jpg">
+            </div>
+            <div class="media-content px-3 align-self-center mbr-white py-2">
+                    <p class="mbr-text testimonial-text mbr-fonts-style display-7">
+                       Loving the additions to the new version of Mobirise web dev app. It's a great, cleanly designed, user-friendly, not-bloated design program. So easy and a pleasure to use
+                    </p>
+                    <p class="mbr-author-name pt-4 mb-2 mbr-fonts-style display-7">
+                       David F.
+                    </p>
+                    <p class="mbr-author-desc mbr-fonts-style display-7">
+                       DEVELOPER
+                    </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="testimonials3 cid-r5NG02P1kz" id="testimonials3-i">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="media-content px-3 align-self-center mbr-white py-2">
+                <p class="mbr-text testimonial-text mbr-fonts-style display-7">
+                   It's really very amazing app that makes me finish html page in 3 minutes that's usually takes more than 1 hours at least from me if i did it from scratch
+                </p>
+                <p class="mbr-author-name pt-4 mb-2 mbr-fonts-style display-7">
+                   Julie C.
+                </p>
+                <p class="mbr-author-desc mbr-fonts-style display-7">
+                   INTERFACE DESIGNER
+                </p>
+            </div>
+
+            <div class="mbr-figure pl-lg-5" style="width: 130%;">
+              <img src="assets/images/face5.jpg">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="testimonials1 cid-r5NkSrsriV" id="testimonials1-9">
+
+    
+
+    
+    <div class="container">
+        <div class="media-container-row">
+            <div class="title col-12 align-center">
+                <h2 class="pb-3 mbr-fonts-style display-2">
+                    Our Professional Consultants</h2>
+                <h3 class="mbr-section-subtitle mbr-light pb-3 mbr-fonts-style display-5"></h3>
+            </div>
+        </div>
+    </div>
+
+    <div class="container pt-3 mt-2">
+        <div class="media-container-row">
+            <div class="mbr-testimonial p-3 align-center col-12 col-md-6 col-lg-4">
+                <div class="panel-item p-3">
+                    <div class="card-block">
+                        <div class="testimonial-photo">
+                            <img src="assets/images/face1.jpg">
+                        </div>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, aspernatur, voluptatibus, atque, tempore molestiae.
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="mbr-author-name mbr-bold mbr-fonts-style display-7">
+                             John Smith
+                        </div>
+                        <small class="mbr-author-desc mbr-italic mbr-light mbr-fonts-style display-7">
+                               Developer
+                        </small>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mbr-testimonial p-3 align-center col-12 col-md-6 col-lg-4">
+                <div class="panel-item p-3">
+                    <div class="card-block">
+                        <div class="testimonial-photo">
+                            <img src="assets/images/face2.jpg">
+                        </div>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, aspernatur, voluptatibus, atque, tempore molestiae.
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="mbr-author-name mbr-bold mbr-fonts-style display-7">
+                             John Smith
+                        </div>
+                        <small class="mbr-author-desc mbr-italic mbr-light mbr-fonts-style display-7">
+                               Developer
+                        </small>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mbr-testimonial p-3 align-center col-12 col-md-6 col-lg-4">
+                <div class="panel-item p-3">
+                    <div class="card-block">
+                        <div class="testimonial-photo">
+                            <img src="assets/images/face3.jpg">
+                        </div>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, aspernatur, voluptatibus, atque, tempore molestiae.
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="mbr-author-name mbr-bold mbr-fonts-style display-7">
+                             John Smith
+                        </div>
+                        <small class="mbr-author-desc mbr-italic mbr-light mbr-fonts-style display-7">
+                               Developer
+                        </small>
+                    </div>
+                </div>
+            </div>
+
+            
+
+            
+
+            
+        </div>
+    </div>   
+</section>
 @endsection
