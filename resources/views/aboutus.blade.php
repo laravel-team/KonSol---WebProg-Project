1bdad369baf6c21594e8e219b8a06600ad082eaa
WilliamH98 <william.hartanto.wh98@gmail.com>
2019-01-01 10:55:40 +0700

    About Us

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,219 @@
+@extends('layout')
+
+@section('title', 'About Us')
+
+@section('content')
+<section class="engine"><a href="https://mobirise.me/t">free amp templates</a></section><section class="header3 cid-rdNEP0aGKJ mbr-parallax-background" id="header3-1w">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.3; background: yellow">
+    </div>
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="mbr-figure" style="width: 100%;">
+                <img src="assets/images/Logo.png" alt="Mobirise">
+            </div>
+
+            <div class="media-content">
+                <h1 class="mbr-section-title mbr-black pb-3 mbr-fonts-style display-1" style="border-bottom: 5px solid black;">
+                    About KonsOL</h1>
+                
+                <div class="mbr-section-text mbr-black pb-3 ">
+                    <p class="mbr-text mbr-fonts-style display-5">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
+                    </p>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="mbr-section content4 cid-rdNFNXVaGJ" id="content4-1y">
+
+    
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="title col-12 col-md-8">
+                <h2 class="align-center pb-3 mbr-fonts-style display-2">
+                    Our Teams</h2>
+                
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="features17 cid-rdNFrfKdfY" id="features17-1x">
+    
+    
+
+    
+    <div class="container-fluid">
+        <div class="media-container-row">
+            <div class="card p-3 col-12 col-md-6 col-lg-2">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/default-pp.jpg" alt="Mobirise">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Team 1
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-2">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/default-pp.jpg" alt="Mobirise">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Team 2
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-2">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/default-pp.jpg" alt="Mobirise">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Team 3
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-2">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/default-pp.jpg" alt="Mobirise">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Team 4
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                           Lorem Ipsum
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="card p-3 col-12 col-md-6 col-lg-2">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/default-pp.jpg" alt="Mobirise">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Team 5
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="card p-3 col-12 col-md-6 col-lg-2">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/default-pp.jpg" alt="Mobirise">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Team 6
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="toggle2 cid-rdNzFCJPvo" id="toggle2-1s">
+    
+        
+
+    
+        <div class="container">
+            <div class="media-container-row">
+                    <div class="toggle-content">
+                        <h2 class="mbr-section-title pb-3 align-left mbr-fonts-style display-2">KonsOL</h2>
+                        
+                        <div id="bootstrap-toggle" class="toggle-panel accordionStyles tab-content pt-5 mt-2">
+                            <div class="card">
+                                <div class="card-header" role="tab" id="headingOne">
+                                    <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-parent="#accordion" data-core="" href="#collapse1_3" aria-expanded="false" aria-controls="collapse1">
+                                        <h4 class="mbr-fonts-style display-5">
+                                            <span class="sign mbr-iconfont mbri-arrow-down inactive"></span> Address
+                                        </h4>
+                                    </a>
+                                </div>
+                                <div id="collapse1_3" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne">
+                                    <div class="panel-body p-4">
+                                        <p class="mbr-fonts-style panel-text display-7">
+                                           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                           tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                           consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                           cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                           proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-header" role="tab" id="headingTwo">
+                                    <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-parent="#accordion" data-core="" href="#collapse2_3" aria-expanded="false" aria-controls="collapse2">
+                                        <h4 class="mbr-fonts-style display-5">
+                                            <span class="sign mbr-iconfont mbri-arrow-down inactive"></span> Contact
+                                        </h4>
+                                    </a>
+                                </div>
+                                <div id="collapse2_3" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingTwo">
+                                    <div class="panel-body p-4">
+                                        <p class="mbr-fonts-style panel-text display-7">
+                                           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                           tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                           consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                           cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                           proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            
+                            
+                            
+                        </div>
+                    </div>
+                <div class="mbr-figure" style="width: 100%;">
+                     <img src="assets/images/background3.jpg" alt="Mobirise">
+                </div>
+            </div>
+        </div>
+</section>
+@endsection
